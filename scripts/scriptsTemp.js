e4af7c2a53b43e15d82302317f564bc4ebe1848c
kevinscircle@aol.com <kevingeoreyes@gmail.com>
2024-07-27 00:41:01 -0700

    add temperature symbol

--- scripts/scriptsTemp.js
+++ scripts/scriptsTemp.js
@@ -6,6 +6,6 @@ var conversion = temperature * (9/5) + 32;
 
 
 document.getElementById('conversion').innerHTML += `
-<p>The conversion of Celsius ${temperature} to Fahrenheit is ${conversion}</p>`;
+<p>The conversion of Celsius ${temperature}&deg to Fahrenheit is ${conversion}&deg</p>`;
 
 }
